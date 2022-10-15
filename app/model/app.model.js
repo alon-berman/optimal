06c438d02b9bf83d8b607f8a04f0a268c14e47e3
Peleg Admi <pelegadmi@gmail.com>
2022-10-15 18:48:34 +0300

    Added mongoose methods and used them in app.model

--- app/model/app.model.js
+++ app/model/app.model.js
@@ -5,58 +5,30 @@ import {MessageSchema} from "../schema/app.schema.js"
 
 module.exports = {
     deleteItem: async function (uuid) {
-        return new Promise((resolve, _) => {
-            try {
-                delete DB[uuid];
-                resolve(uuid);
-            } catch {
-                resolve(null);
-            }
-        });
+        MessageSchema.methods.delete(uuid);
     },
 
     saveItem: async function (data) {
-            console.log("Saved Object")
-            let uid = generateID();
+        console.log("Saved Object")
+        let uid = generateID();
 
-            const schema = {
-                _id: uid, message: data.message, author: data.author, category: data.category,
-                recipient: data.recepient, max_retries_to_send: data.max_retries_to_send
-            };
-            const Model = mongoose.model('MessageSchema', schema);
-
-            const doc = new Model();
-            doc._id = schema._id;
-            await doc.save(); // works
+        const schema = {
+            _id: uid, message: data.message, author: data.author, category: data.category,
+            recipient: data.recepient, max_retries_to_send: data.max_retries_to_send
+        };
+        MessageSchema.methods.create(schema);
     },
 
     getById: function (uuid) {
-        return new Promise((resolve, _) => {
-            try {
-                resolve(DB[uuid]);
-            } catch (error) {
-                resolve(null);
-            }
-        });
+        return MessageSchema.methods.findType(uuid);
     },
 
     getAll: function () {
-        return new Promise((resolve, _) => {
-            console.log(DB)
-            resolve(DB);
-        });
+        return MessageSchema.methods.findAll();
     },
 
-    updateExisting: function (uuid, data) {
-        return new Promise((resolve, _) => {
-            if (DB.hasOwnProperty(uuid)) {
-                DB[uuid]['message'] = data;
-                resolve(uuid);
-            } else {
-                console.log("Failed to find message")
-                resolve(null);
-            }
-        })
+    updateExisting: function (uuid, newObj) {
+        MessageSchema.methods.update(uuid, newObj);
     }
 }
 
--- app/schema/app.schema.js
+++ app/schema/app.schema.js
@@ -1,12 +1,29 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+
+const {Schema} = mongoose;
 
 
 export const MessageSchema = new Schema({
     _id: Number, // represents Date.Now()
-    message:  String, // String is shorthand for {type: String}
+    message: String, // String is shorthand for {type: String}
     author: String,
-    category:   String,
+    category: String,
     recepient: String,
     max_retries_to_send: Number,
 });
+
+MessageSchema.methods.create = async function (obj) {
+    return mongoose.model('MessageSchema').create(obj);
+}
+MessageSchema.methods.findType = function (uuid) {
+    return mongoose.model('MessageSchema').findOne({_id: uuid});
+}
+MessageSchema.methods.findAll = function () {
+    return mongoose.model('MessageSchema').find();
+}
+MessageSchema.methods.update = function (uuid, newObj) {
+    return mongoose.model('MessageSchema').updateOne({_id: uuid}, newObj);
+}
+MessageSchema.methods.delete = function (uuid) {
+    return mongoose.model('MessageSchema').deleteOne({_id: uuid});
+}
